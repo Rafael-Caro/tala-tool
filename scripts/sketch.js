d87ec2cc683abb34484b14c2b9f3237d66238cdc
Rafael Caro Repetto <rafael.caro.repetto@gmail.com>
2019-01-12 14:20:07 +0100

    Added border to cursor

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -311,7 +311,8 @@ function CreateCursor () {
   }
   this.display = function () {
     fill("red");
-    noStroke();
+    stroke(50);
+    strokeWeight(1);
     ellipse(this.x, this.y, 5, 5)
   }
 }
